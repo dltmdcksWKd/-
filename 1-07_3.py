82a96c358e9c420c656725aa21dd361c0a61283a
dltmdcksWKd <97208404+dltmdcksWKd@users.noreply.github.com>
2022-01-06 12:27:34 +0900

    Add files via upload

---
+++ 1-07_3.py
@@ -0,0 +1,10 @@
+a="hallo prayer?"
+b="e"
+c="l"
+d="!"
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
+print(a[0]+b+a[2:7]+c+a[8:12]+d)
