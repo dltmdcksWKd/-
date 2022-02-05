fe0c3306f4f97e3bc77edc57e5f39138a66ced47
dltmdcksWKd <97208404+dltmdcksWKd@users.noreply.github.com>
2022-02-05 17:48:32 +0900

    Add files via upload

---
+++ 2-05.py
@@ -0,0 +1,23 @@
+# a = int(input())
+# b=[]
+# c=0
+# while a >= 2:
+#     c=c+1
+#     d=int(input())
+#     b.append(d)
+#     if c == a :
+#         break
+# b.sort()
+# print(b)
+# print(b[0],b[-1])
+
+
+
+# a = int(input())
+# b=[]
+# for i in range(a):
+#     d=int(input())
+#     b.append(d)
+# b.sort()
+# print(b)
+# print(b[0],b[-1])
---
+++ 2-05_2.py
@@ -0,0 +1,57 @@
+#튜플
+# t1=()
+# t2=(1,)
+# t3=(1,2,3)
+# t4=1,2,3
+# t5=('a','b','c'('ab','cd'))
+# t1=('a','b','c')
+# print(t1)
+# t1=('b','d','c')
+# print(t1)
+
+# t1=(1,2,'a','b')
+# print(t1[0])
+# print(t1[3])
+# print(t1[1:])
+# a=list(t1)
+# print(a)
+# print(type(a))
+
+# t1=(1,2,'a','b')
+# t2=(3,4)
+# print(t1+t2)
+# print(t1*3)
+# print(len(t1))
+
+#딕셔너리 여러개 값을 한꺼번에 모으고 관련된 정보를 서로 연관시켜 놓는다
+
+# sport={"김연아":"피겨스케이팅","류현진":"야구","손흥민":"축구"}
+# print(sport)
+# print(type(sport))
+
+# a={1:'hi'}
+# a1={'a':[1,2,3]}
+# print(a)
+# print(a1)
+
+# a={1:'a'}
+# print(a)
+# a[2]='b'
+# a['name']='son'
+# a[3]=[1,2,3]
+# print(a)
+# del a['name']
+# print(a)
+# print(len(a))
+
+# grade={'son':7, 'kane':10}
+# print(grade['son'])
+# print(grade['kane'])
+
+# a={1:'a', 2:'b'}
+# print(a[1])
+# print(a[2])
+
+# a={'a':1, 'b':2}
+# print(a['a'])
+# print(a['b'])
