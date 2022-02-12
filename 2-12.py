46bd8cbcac3cf49b5a6151855a9d9c9a7ae2044c
dltmdcksWKd <97208404+dltmdcksWKd@users.noreply.github.com>
2022-02-12 17:51:02 +0900

    Add files via upload

---
+++ 2-12.py
@@ -0,0 +1,63 @@
+# a=['a','b','c','d']
+# a[1]='f'
+# print(a)
+#
+# del a[3]
+# print(a)
+#
+# print(a[2])
+# print(a[1:])
+# print(a[::1])
+#
+# b=['e','f']
+# print(a+b)
+# print(a*3)
+#
+# a=[4,1,3,2]
+# a.append(5)
+# print(a)
+#
+# a.sort()
+# print(a)
+#
+# a.reverse()
+# print(a)
+#
+# print(a.index(4))
+#
+# a.insert(0,6)
+# print(a)
+#
+# a.remove(4)
+# print(a)
+#
+# print(a.pop(2),a)
+# print(a.pop(),a)
+#
+# print(a.count(4))
+# print(a.count(0))
+#
+# b=[5,7]
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)
+#
+# a=(4,1,3,2,4)
+# print(len(a))
+#
+# d={"apple":'사과',"banana":'바나나',"3":'삼'}
+# d[4]='사'
+# print(d)
+# del d[4]
+# print(d)
+#
+# print(d['banana'])
+#
+# key=d.key()
+# value=d.value()
+# item=d.item()
+#
+# print(list(key))
+# print(list(value))
+# print(list(item))
---
+++ 2-12_2.py
@@ -0,0 +1,90 @@
+# letters='prython'
+# print(letters[0],letters[2])
+#
+# license_plate="24가 2210"
+# print(license_plate[4:8])
+# print(license_plate[-4:])
+#
+# string="홀짝홀짝홀짝"
+# print(string[1::2])    (ㅇㅁㅇ)
+#
+# string="PYTHON"
+# print(string[::-1])
+#
+# phone_number="010-1111-2222"
+# print(phone_number.replce('-',''))
+#
+# string = 'abcdfe2a354a32a'
+# print(string.replace('a', 'A'))
+#
+# t1 = 'python'
+# t2 = 'java'
+# t3 = t1 + ' ' + t2 + ' '
+# print(t3 * 4)
+#
+# name1 = "김민수"
+# age1 = 10
+# name2 = "이철희"
+# age2 = 13
+#
+# print("이름: %s 나이: %d" % (name1, age1))
+# print("이름: %s 나이: %d" % (name2, age2))
+#
+# print("이름: {} 나이: {}".format(name1, age1))
+# print("이름: {} 나이: {}".format(name2, age2))
+#
+# print(f"이름: {name1} 나이: {age1}")
+# print(f"이름: {name2} 나이: {age2}")
+#
+# a=int(input())
+# if a + 20 <= 255:
+#     a=a+20
+#     print(a)
+# else:
+#     print(255)
+#
+# a=int(input())
+# if a - 20 <= 0:
+#     print(0)
+# elif a - 20 <= 255:
+#     a=a-20
+#     print(a)
+#
+# else:
+#     print(255)
+#
+# fruit = ["사과", "포도", "홍시"]
+# user = input("좋아하는 과일은?")
+# if user in fruit:
+#     print("정답입니다.")
+# else:
+#     print("오답입니다.")
+#
+# fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+# user = input("제가좋아하는계절은: ")
+# if user in fruit:
+#     print("정답입니다.")
+# else:
+#     print("오답입니다.")
+#
+# fruit = {"봄" : "딸기", "여름" : "토마토", "가을" : "사과"}
+# user = input("제가좋아하는과일일: ")
+# if user in fruit.values:
+#     print("정답입니다
+# else:.")
+#     print("오답입니다.")
+#
+# 변수 = 10
+# print(변수)
+# print("-------")
+#
+# 변수 = 20
+# print(변수)
+# print("-------")
+#
+# 변수 = 30
+# print(변수)
+# print("-------")
+#
+# for num in range(3, 31, 3):
+#     print (num)
